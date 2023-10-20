d650625a09a1b72584b7ae6cf05a02efdbc84854
sunzhigang <454661248@qq.com>
2023-10-20 20:46:10 +0800

    添加moneythings与anydown js

---
+++ script/MoneyThings/MoneyThings.js
@@ -0,0 +1,53 @@
+/*************************************
+
+项目名称：MoneyThings-记账
+下载地址：https://t.cn/A6pg9nPW
+项目名称：SalesCat-RevenueCat客户端
+下载地址：https://t.cn/A6pFPFKM
+项目名称：MatrixClocca-矩阵时钟
+下载地址：https://t.cn/A6pFhtw1
+脚本作者：chxm1023
+
+**************************************
+
+[rewrite_local]
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/MoneyThings.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/MoneyThings.js
+
+[mitm]
+hostname = api.revenuecat.com
+
+*************************************/
+
+
+const pro = {};
+const pro1 = JSON.parse(typeof $response != "undefined" && $response.body || null);const app = 'cm';const list = {'cm':{name: 'Premium', id: 'com.lishaohui.cashflow.lifetime'}};
+const data = {
+	"Author": "chxm1023",
+	"Telegram" : "https://t.me/chxm1023",
+	"warning": "仅供学习，禁止转载或售卖",
+	"original_purchase_date": "2022-09-09T09:09:09Z",
+	"purchase_date": "2022-09-09T09:09:09Z",
+	"expires_date": "2099-09-09T09:09:09Z",
+	"ownership_type": "PURCHASED"
+};
+
+if (typeof $response == "undefined") {
+	delete $request.headers["x-revenuecat-etag"];
+	delete $request.headers["X-RevenueCat-ETag"];
+	pro.headers = $request.headers;
+} else if (pro1 && pro1.subscriber) {
+	pro1.subscriber.subscriptions = pro1.subscriber.subscriptions || {};
+	pro1.subscriber.entitlement = pro1.subscriber.entitlement || {};
+     for (const i in list) {
+if (new RegExp(`^${i}`, `i`).test(app)) {
+	pro1.subscriber.subscriptions[list[i].id] = data;
+	pro1.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+	pro1.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+			break;
+		}
+	}
+    pro.body = JSON.stringify(pro1);
+}
+
+$done(pro);
