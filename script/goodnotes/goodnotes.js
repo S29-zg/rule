7cbcb6a02fe7c14fa7fc6c82891941c0c094508f
S29-zg <84295019+S29-zg@users.noreply.github.com>
2023-11-12 17:14:55 +0800

    goodnotes.js
    添加goodnotes.js

--- script/goodnotes/goodnotes.js
+++ script/goodnotes/goodnotes.js
@@ -5,8 +5,8 @@ author：chxm1023
 
 **************************************
 [rewrite_local]
-^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body http://git.yycm.link/chxm1023/Rewrite/raw/main/gg.js
-^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header http://git.yycm.link/chxm1023/Rewrite/raw/main/gg.js
+^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-response-body https://raw.githubusercontent.com/S29-zg/rule/main/script/goodnotes/goodnotes.js
+^https:\/\/isi\..*\.g.*\.(com\..*|com)\/.+\/(receipts$|subscribers\/?(.*?)*$) url script-request-header https://raw.githubusercontent.com/S29-zg/rule/main/script/goodnotes/goodnotes.js
 
 [mitm]
 hostname = isi.*.g*.com*
