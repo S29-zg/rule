08beb644be570d33fc77a19f56864d57bfd5091a
sunzhigang <454661248@qq.com>
2024-03-08 21:46:12 +0800

    update YiYan

--- script/s29/YiYanOfDay.js
+++ script/s29/YiYanOfDay.js
@@ -1,16 +1,25 @@
-$httpClient.get('https://v1.hitokoto.cn?c=a&encode=json', function(error, response, data) {   
+const $=new env("YiYan");
+$.type=$.getdata("type")?$.getdata("type"):"";
+
+
+$httpClient.get(
+  "https://v1.hitokoto.cn?c=${$.type}&encode=json",
+  function (error, response, data) {
     // 检查是否有错误发生
     if (error) {
-        console.error("请求错误:", error);
-        return;
+      console.error("请求错误:", error);
+      return;
     }
-    
+
     // 解析 JSON 数据
     var jsonData = JSON.parse(data);
-    
+
     // 提取句子内容
     var hitokoto = jsonData.hitokoto;
-    
+    var author=jsonData.from_who;
+    var tp=jsonData.type;
+
     // 发送通知
-    $notification.post('每日一言', 'by s29', hitokoto);
-});
+    $notification.post("每日一言-"+tp, author, hitokoto);
+  }
+);
