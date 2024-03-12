1d71f6477fa9fdcf17aab450c350cc3387e531fa
sunzhigang <454661248@qq.com>
2024-03-12 20:31:42 +0800

    add Auto_Join_TF

--- script/TFAccount/Auto_join_TF.js
+++ script/TFAccount/Auto_join_TF.js
@@ -18,7 +18,6 @@
   }
   $done()
 })()
-
 function autoPost(ID) {
   let Key = $persistentStore.read('key')
   let testurl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/'
@@ -70,3 +69,5 @@ function autoPost(ID) {
     })
   })
 }
+
+函数 autoPost(ID) { let Key = $persistentStore.read('key') let testurl = 'https://testflight.apple.com/v3/accounts/' + Key + '/ru/' let header = { ' X-Session-Id': `${$persistentStore.read('session_id')}`, 'X-Session-Digest': `${$persistentStore.read('session_digest')}`, 'X-Request- Id': `${$persistentStore.read('request_id')}`, 'User-Agent': `${$persistentStore.read('tf_ua')}`, } return new Promise(function (resolve) { $ httpClient.get({ url: testurl + ID, headers: header }, function (error, resp, data) { if (error == null) { if (resp.status == 404) { ids = $persistentStore.read( 'APP_ID').split(',') ids = ids.filter(ids => ids !== ID) $persistentStore.write(ids.toString(), 'APP_ID') console.log(ID + ' ' + '不存在该 TestFlight，已自动删除该 APP_ID') $notification.post(ID, '不存在该 TestFlight', '已自动删除该 APP_ID') resolve() } else { let jsonData = JSON.parse(data) if (jsonData.data == null) { console.log(ID + ' ' + jsonData.messages[0].message) resolve() } else if (jsonData.data.status == 'FULL') { console.log (jsonData.data.app.name + ' ' + ID + ' ' + jsonData.data.message) resolve() } else { $httpClient.post({ url: testurl + ID + '/accept', headers: header } , function (error, resp, body) { let jsonBody = JSON.parse(body) $notification.post(jsonBody.data.name, 'TestFlight 加入成功', '') console.log(jsonBody.data.name + ' TestFlight 加入成功') ids = $persistentStore.read('APP_ID').split(',') ids = ids.filter(ids => ids !== ID) $persistentStore.write(ids.toString(), ' APP_ID')resolve() }) } } } else { if (error == '请求超时。') {resolve() } else { $notification.post('自动加入 TestFlight', error, '') console .log(ID + ' ' + 错误) 解析() } } }) }) }
